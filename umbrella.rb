bce35c3c4cd58c4f3f0bcaffbd3bd02decc362dc
thisiscindy24 <cindyannwu@gmail.com>
2025-01-23 17:23:41 +0000

    user location

--- umbrella.rb
+++ umbrella.rb
@@ -6,6 +6,9 @@ require "pry-byebug"
 require "http"
 require "dotenv"
 
-pp "Where are you located?"
-user_location = gets
+# pp "Where are you located?"
+user_location = gets.chomp
 pp user_location
+
+gmap_key = ENV.fetch("GMAPS_KEY")
+gmap_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmap_key}"
