8844bae3e3af71380ef8a50ae0e5ee0e8a47edbe
thisiscindy24 <cindyannwu@gmail.com>
2025-01-23 17:06:41 +0000

    location question

---
+++ env_test.rb
@@ -0,0 +1,6 @@
+# /env_test.rb
+require "dotenv/load"
+
+pp ENV.fetch("GMAPS_KEY")
+pp ENV.fetch("OPENAI_KEY")
+pp ENV.fetch("PIRATE_WEATHER_KEY")
--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,11 @@
 # Write your soltuion here!
+
+require "active_support/all"
+require "awesome_print"
+require "pry-byebug"
+require "http"
+require "dotenv"
+
+pp "Where are you located?"
+user_location = gets
+pp user_location
